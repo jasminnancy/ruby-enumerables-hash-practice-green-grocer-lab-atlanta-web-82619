acdb170e4ab17e6902fc23b9e310451ee4ac41dd
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-08-19 16:58:19 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -33,8 +33,8 @@ end
 
 def apply_clearance(cart)
   cart.each do |item, value|
-     if cart[item[:item]][:clearance] == TRUE
-       cart[item[:item]][:price] = (cart[item[:item]][:price] *0.8).round(2)
+     if value[:clearance] == TRUE
+       value[:price] = (value[:price] *0.8).round(2)
      end
    end
    return cart
